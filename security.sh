a7c9b16b2a59df930798b1bbf30c8b44f2bed19b
rmkn <bubblun@gmail.com>
2016-11-28 13:30:39 +0900

    disable alias/scriptalias

--- security.sh
+++ security.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 sed -i -e 's/expose_php = On/expose_php = Off/' /etc/opt/remi/php70/php.ini
-sed -i -e 's/ServerTokens OS/ServerTokens Prod/' /etc/httpd/conf/httpd.conf
-sed -i -e 's/ServerSignature On/ServerSignature Off/' /etc/httpd/conf/httpd.conf
-sed -i -e 's/AddDefaultCharset UTF-8/AddDefaultCharset Off/' /etc/httpd/conf/httpd.conf
+sed -i -e 's/^ServerTokens OS/ServerTokens Prod/' /etc/httpd/conf/httpd.conf
+sed -i -e 's/^ServerSignature On/ServerSignature Off/' /etc/httpd/conf/httpd.conf
+sed -i -e 's/^AddDefaultCharset UTF-8/AddDefaultCharset Off/' /etc/httpd/conf/httpd.conf
 sed -i -e 's/Options Indexes/Options/' /etc/httpd/conf/httpd.conf
+sed -i -e 's/^Alias/#Alias/' /etc/httpd/conf/httpd.conf
+sed -i -e 's/^ScriptAlias/#ScriptAlias/' /etc/httpd/conf/httpd.conf
 
 cat <<EOT > /etc/httpd/conf.d/security.conf
 TraceEnable Off
